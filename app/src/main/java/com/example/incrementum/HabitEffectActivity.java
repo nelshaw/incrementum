8e797fee06c717f331f97c38dff6d270c52bc58b
hadeelelmadhoon <hadeelelmadhoon@users.noreply.github.com>
2020-02-09 13:17:43 -0500

    add app logic to button in habit effect

--- app/src/main/java/com/example/incrementum/HabitEffectActivity.java
+++ app/src/main/java/com/example/incrementum/HabitEffectActivity.java
@@ -9,11 +9,46 @@ import android.widget.Button;
 
 public class HabitEffectActivity extends AppCompatActivity {
 
+    Button nButton;
+    Button pButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_effect);
 
+        // find button by id
+        nButton = findViewById(R.id.negative_button);
+        pButton = findViewById(R.id.positive_button);
+
+        // on click function negative button
+        nButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openJournalActivity();
+            }
+        });
+
+        // on click function positive button
+        pButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openHomeActivity();
+            }
+        });
+
+    }
+
+    // open journal
+    public void openJournalActivity(){
+        Intent intent = new Intent(this, JournalActivity.class);
+        startActivity(intent);
+    }
+
+    // open home
+    public void openHomeActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
 }
