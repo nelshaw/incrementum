9042ac943ce1ad2c524ebe9c20c48519166f6929
zakhodgson1 <zhodgson@uwo.ca>
2020-02-27 13:11:40 -0500

    Functional db connection in MainActivity

--- app/src/main/java/com/example/incrementum/MainActivity.java
+++ app/src/main/java/com/example/incrementum/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.mongodb.stitch.android.core.Stitch;
@@ -21,10 +22,14 @@ import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient;
 import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection;
 import com.mongodb.stitch.core.auth.providers.anonymous.AnonymousCredential;
 import com.mongodb.stitch.core.services.mongodb.remote.RemoteInsertOneResult;
+import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateOptions;
+import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateResult;
 
 import org.bson.Document;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,37 +45,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         title = findViewById(R.id.title);
-        Stitch.initializeDefaultAppClient("incrementum-xjkms");
-      StitchAppClient client = Stitch.getDefaultAppClient();
 
-      client.getAuth().loginWithCredential(new AnonymousCredential())
-        .continueWithTask(new Continuation<StitchUser, Task<Void>>() {
-          @Override
-          public Task<Void> then(@NonNull Task<StitchUser> task) throws Exception {
-            return null;
-          }
+
+        final StitchAppClient client =
+                Stitch.initializeDefaultAppClient("incrementum-xjkms");
+
+        final RemoteMongoClient mongoClient =
+                client.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
+
+        final RemoteMongoCollection<Document> coll =
+                mongoClient.getDatabase("Incrementum").getCollection("test");
+
+        client.getAuth().loginWithCredential(new AnonymousCredential()).continueWithTask(
+                new Continuation<StitchUser, Task<RemoteUpdateResult>>() {
+
+                    @Override
+                    public Task<RemoteUpdateResult> then(@NonNull Task<StitchUser> task) throws Exception {
+                        if (!task.isSuccessful()) {
+                            Log.e("STITCH", "Login failed!");
+                            throw task.getException();
+                        }
+
+                        final Document updateDoc = new Document(
+                                "owner_id",
+                                task.getResult().getId()
+                        );
+
+                        updateDoc.put("number", 25);
+                        return coll.updateOne(
+                                null, updateDoc, new RemoteUpdateOptions().upsert(true)
+                        );
+                    }
+                }
+        ).continueWithTask(new Continuation<RemoteUpdateResult, Task<List<Document>>>() {
+            @Override
+            public Task<List<Document>> then(@NonNull Task<RemoteUpdateResult> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    Log.e("STITCH", "Update failed!");
+                    throw task.getException();
+                }
+                List<Document> docs = new ArrayList<>();
+                return coll
+                        .find(new Document("owner_id", client.getAuth().getUser().getId()))
+                        .limit(100)
+                        .into(docs);
+            }
+        }).addOnCompleteListener(new OnCompleteListener<List<Document>>() {
+            @Override
+            public void onComplete(@NonNull Task<List<Document>> task) {
+                if (task.isSuccessful()) {
+                    Log.d("STITCH", "Found docs: " + task.getResult().toString());
+                    return;
+                }
+                Log.e("STITCH", "Error: " + task.getException().toString());
+                task.getException().printStackTrace();
+            }
         });
-
-      final RemoteMongoClient mongoClient = client
-        .getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
-
-      Document doc = new Document().append("time", new Date());
-
-      RemoteMongoCollection<Document> collection = mongoClient.getDatabase("Incrementum")
-        .getCollection("Users");
-
-      Log.d("TEST" , "TEST");
-
-      collection.insertOne(doc)
-        .addOnSuccessListener(new OnSuccessListener<RemoteInsertOneResult>() {
-                                @Override
-                                public void onSuccess(RemoteInsertOneResult remoteInsertOneResult) {
-                                  Log.d("STITCH", "One doc inserted");
-                                }
-
-                              });
-
-
 
 
 
