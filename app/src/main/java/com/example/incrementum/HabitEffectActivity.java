248453da9016ae8a7bb72673ab4f20e2d5b259e1
hadeelelmadhoon <hadeelelmadhoon@users.noreply.github.com>
2020-02-28 17:20:27 -0500

    test

--- app/src/main/java/com/example/incrementum/HabitEffectActivity.java
+++ app/src/main/java/com/example/incrementum/HabitEffectActivity.java
@@ -2,16 +2,27 @@ package com.example.incrementum;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.mongodb.stitch.android.core.Stitch;
+import com.mongodb.stitch.android.core.StitchAppClient;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteFindIterable;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection;
+
+import org.bson.Document;
+
 
 public class HabitEffectActivity extends AppCompatActivity {
 
     Button nButton;
     Button pButton;
     Button dialogButton;
+    String[] quotes;
 
     public enum Type{
         NEGATIVE,
@@ -32,6 +43,7 @@ public class HabitEffectActivity extends AppCompatActivity {
         nButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                getNegativeQuotes();
                 String quote = "neg quote";
                 Type type = Type.NEGATIVE;
                 openDialog(type, quote);
@@ -86,6 +98,31 @@ public class HabitEffectActivity extends AppCompatActivity {
 //                    openHabitActivity();
 //            }
 //        }, 5000);
+    }
+
+    public void getNegativeQuotes(){
+        final StitchAppClient client =
+                Stitch.getAppClient("incrementum-xjkms");
+
+        final RemoteMongoClient mongoClient =
+                client.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
+
+        final RemoteMongoCollection<Document> coll =
+                mongoClient.getDatabase("Incrementum").getCollection("Quotes");
+
+//        Document filterDoc = new Document().append("Type", "Positive");
+
+        RemoteFindIterable results = coll.find(new Document());
+
+        Log.d("sjnd-------------", "start");
+
+        results.forEach(item -> {
+            Log.d("---------ITEM------", item.toString());
+
+        });
+
+        Log.d("sjnd-------------", "end");
+
     }
 
 }
