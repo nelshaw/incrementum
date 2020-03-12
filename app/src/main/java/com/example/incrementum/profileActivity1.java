aad00ea6333a6530b7bc20ee15c6add4f82763a3
naswad <46467937+naswad@users.noreply.github.com>
2020-03-12 18:43:41 -0400

    fixes crash

--- app/src/main/java/com/example/incrementum/profileActivity1.java
+++ app/src/main/java/com/example/incrementum/profileActivity1.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import butterknife.InjectView;
 
 public class profileActivity1 extends AppCompatActivity {
 
-    @InjectView(R.id.emailText) TextView _emailText;
+    TextView _emailText;
 
 
 
@@ -19,13 +21,14 @@ public class profileActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        _emailText = findViewById(R.id.emailText);
         Intent intent = getIntent();
         String email = intent.getStringExtra("email");
+        Toast.makeText(getBaseContext(),email, Toast.LENGTH_LONG).show();
 
 
 
-       // _emailText.setText(email);
-
+        
+        _emailText.setText(email);
     }
 }
