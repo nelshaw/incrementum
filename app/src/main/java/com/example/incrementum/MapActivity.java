e5ee5bf66e404b7c86555fe57cf740f1c547352d
Nadeen E <nelshaw@uwo.ca>
2020-03-15 18:07:08 -0400

    Update other activities front end

--- app/src/main/java/com/example/incrementum/MapActivity.java
+++ app/src/main/java/com/example/incrementum/MapActivity.java
@@ -24,9 +24,6 @@ public class MapActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_map);
 
-    habit = findViewById(R.id.habit);
-    journal = findViewById(R.id.journal);
-    logout = findViewById(R.id.logout);
     map = findViewById(R.id.map);
     mapClickCounter = 0;
     map.setImageDrawable(getResources().getDrawable(R.drawable.map0));
@@ -69,27 +66,6 @@ public class MapActivity extends AppCompatActivity {
       }
     });
 
-    habit.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        openViewHabitActivity();
-      }
-    });
-
-    journal.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        openViewJournalActivity();
-      }
-    });
-
-    logout.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        logout();
-      }
-    });
-
     //Initalize and Assign Value
     BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
@@ -126,20 +102,5 @@ public class MapActivity extends AppCompatActivity {
       }
     });
 
-  }
-
-  public void openViewHabitActivity() {
-    Intent intent = new Intent(this, ViewHabitActivity.class);
-    startActivity(intent);
-  }
-
-  public void openViewJournalActivity() {
-    Intent intent = new Intent(this, ViewJournalActivity.class);
-    startActivity(intent);
-  }
-
-  public void logout() {
-    Intent intent = new Intent(this, LoginActivity.class);
-    startActivity(intent);
   }
 }
