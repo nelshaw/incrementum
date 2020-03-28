f6186e4905f4bc0c9ddbc3a26135587e6b48d265
jlad7 <jlad7@uwo.ca>
2020-03-27 22:40:35 -0400

    Added Profile Picture

--- app/src/main/java/com/example/incrementum/ProfileActivity.java
+++ app/src/main/java/com/example/incrementum/ProfileActivity.java
@@ -1,14 +1,25 @@
 package com.example.incrementum;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,13 +42,38 @@ public class ProfileActivity extends AppCompatActivity {
   String userName;
   String email;
   Button refresh;
+  ImageView profilePicture;
+  Button addNewProfilePicture;
 
+  private static final int PERMISSION_REQUEST = 0;
+  private static final int RESULT_LOAD_IMAGE = 1;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_profile);
+
+    String[] permissionsNeeded = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    != PackageManager.PERMISSION_GRANTED){
+     // requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST);
+      requestPermissions(permissionsNeeded, PERMISSION_REQUEST);
+    }
+
     _emailText = findViewById(R.id.emailText);
     _emailText.setText(email);
+    profilePicture = (ImageView)  findViewById(R.id.profilePic);
+    addNewProfilePicture = (Button)  findViewById(R.id.addProfilePicture);
+
+//    addNewProfilePicture.setOnClickListener(new View.OnClickListener() {
+//      @Override
+//      public void onClick(View v) {
+//        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//        startActivityForResult(i, RESULT_LOAD_IMAGE);
+//      }
+//    });
+
+
 
     refresh = findViewById(R.id.refresh);
     refresh.setOnClickListener(new View.OnClickListener() {
@@ -99,6 +135,39 @@ public class ProfileActivity extends AppCompatActivity {
 
   }
 
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    switch (requestCode){
+      case PERMISSION_REQUEST:
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+          Toast.makeText(this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+          Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.O)
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    switch (requestCode){
+      case RESULT_LOAD_IMAGE:
+        if (resultCode == RESULT_OK){
+          Uri selectedImage = data.getData();
+          String[] filePathColumn = {MediaStore.Images.Media.DATA};
+          Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null);
+          cursor.moveToFirst();
+          int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+          String picturePath = cursor.getString(columnIndex);
+          cursor.close();
+          profilePicture.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
+    }
+  }
 
   public void getData() {
     RemoteFindIterable<Document> results;
